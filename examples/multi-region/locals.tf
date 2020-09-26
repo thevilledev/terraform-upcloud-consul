6d046e859da5ab78affbfe7c2c5fde718ef3f583
Ville Törhönen <ville@torhonen.fi>
2020-09-26 23:08:35 +0300

    add locals

---
+++ examples/multi-region/locals.tf
@@ -0,0 +1,7 @@
+locals {
+    upcloud_api_username = "ENTER_UPCLOUD_API_USERNAME_HERE"
+    upcloud_api_password = "ENTER_UPCLOUD_API_PASSWORD_HERE"
+    login_user = "tf"
+    login_keys = ["ENTER_KEYS_HERE"]
+
+}
